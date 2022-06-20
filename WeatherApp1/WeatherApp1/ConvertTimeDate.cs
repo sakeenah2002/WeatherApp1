d8d0dfe4cb1432b0b5b3a384a15c11f2936e274d
Raven-voegt <102142329+Raven-voegt@users.noreply.github.com>
2022-06-20 09:58:44 +0200

    Add date and time converter

---
+++ WeatherApp1/WeatherApp1/ConvertTimeDate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using Xamarin.Forms;
+using System.Globalization;
+using System.Text;
+
+namespace WeatherApp1
+{
+    public class ConvertTimeDate : IValueConverter
+
+    {
+        DateTime _time = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long dateTime = (long)value;
+            return $"{_time.AddSeconds(dateTime).ToString()} UTC";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WeatherApp1/WeatherApp1/RestServices.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WeatherApp1;
+
+namespace WeatherApp
+{
+    public class RestServices
+    {
+        HttpClient _client;
+
+        public RestServices()
+        {
+            _client = new HttpClient();
+        }
+
+        //getting the weather data from the text input
+        public async Task<Datatags1> GetDatatags1(string query)
+        {
+            Datatags1 Datatags = null;
+            try
+            {
+                var response = await _client.GetAsync(query);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Datatags = JsonConvert.DeserializeObject<Datatags1>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("\t\tERROR {0}", ex.Message);
+            }
+
+            return Datatags;
+        }
+    }
+}
