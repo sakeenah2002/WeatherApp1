11aaaa014d0eb0599ed49bb8318324449aaca194
Raven-voegt <102142329+Raven-voegt@users.noreply.github.com>
2022-06-20 10:29:51 +0200

    added constants

---
+++ WeatherApp1/WeatherApp1/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp1
+{
+    public static class Constants
+    {
+        // linking the coordinates to the app.
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "a9956d3cbc86866548330347d551ed14";
+    }
+}
